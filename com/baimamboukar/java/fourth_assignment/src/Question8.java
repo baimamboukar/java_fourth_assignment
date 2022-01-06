77c807fb2128a43d102496c11173fdf4b0aeac7b
baimamboukar <baimamboukar@gmail.com>
2022-01-06 23:07:05 +0100

    committing Question8.java

---
+++ com/baimamboukar/java/fourth_assignment/src/Question8.java
@@ -0,0 +1,30 @@
+package com.baimamboukar.java.fourth_assignment.src;
+
+/**
+ * COURSE: JAVA PROGRAMMING I | ICT UNIVERSITY | FALL 2021
+ * INSTRUCTOR: Eng. DANIEL MOUNE
+ * ..................................................................
+ * ⚝ NAME: BAIMAM BOUKAR JEAN JACQUES
+ * ⚝ MATRIC NO: ICTU20201685
+ * ⚝ EMAIL: baimamboukar@gmail.com
+ * ⚝ GITHUB: github.com/baimamboukar
+ * ⚝ LINKEDIN: linkedin.com/in/baimamboukar
+ * ..................................................................
+ */
+
+import java.util.Scanner;
+
+public class Question8 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		// Prompt the user to enter an ASCII code
+		// (an integer between 0 and 127)
+		System.out.print("Enter an ASCII code: ");
+		int i = input.nextInt();
+
+		// Display ASCII code as character
+		System.out.println((char) i);
+		input.close();
+	}
+}
