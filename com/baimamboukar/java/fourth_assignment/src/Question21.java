d3e6cf597d9016590edc362c19c3419c56465f35
baimamboukar <baimamboukar@gmail.com>
2022-01-06 23:06:12 +0100

    committing Question21.java

---
+++ com/baimamboukar/java/fourth_assignment/src/Question21.java
@@ -0,0 +1,44 @@
+package com.baimamboukar.java.fourth_assignment.src;
+
+/**
+ * COURSE: JAVA PROGRAMMING I | ICT UNIVERSITY | FALL 2021
+ * INSTRUCTOR: Eng. DANIEL MOUNE
+ * ..................................................................
+ * ⚝ NAME: BAIMAM BOUKAR JEAN JACQUES
+ * ⚝ MATRIC NO: ICTU20201685
+ * ⚝ EMAIL: baimamboukar@gmail.com
+ * ⚝ GITHUB: github.com/baimamboukar
+ * ⚝ LINKEDIN: linkedin.com/in/baimamboukar
+ * ..................................................................
+ */
+
+import java.util.Scanner;
+
+public class Question21 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		// Prompt the user to enter a Social Security number
+		System.out.print("Enter a SSN: ");
+		String ssn = input.nextLine();
+
+		// Check whether the input is valid
+		boolean isValid = (ssn.length() == 11) &&
+				(Character.isDigit(ssn.charAt(0))) &&
+				(Character.isDigit(ssn.charAt(1))) &&
+				(Character.isDigit(ssn.charAt(2))) &&
+				(ssn.charAt(3) == '-') &&
+				(Character.isDigit(ssn.charAt(4))) &&
+				(Character.isDigit(ssn.charAt(5))) &&
+				(Character.isDigit(ssn.charAt(7))) &&
+				(ssn.charAt(6) == '-') &&
+				(Character.isDigit(ssn.charAt(8))) &&
+				(Character.isDigit(ssn.charAt(9))) &&
+				(Character.isDigit(ssn.charAt(10)));
+
+		// Display result
+		System.out.println(ssn + " is " + ((isValid) ? "a valid " : "an invalid ")
+				+ "social security number");
+		input.close();
+	}
+}
