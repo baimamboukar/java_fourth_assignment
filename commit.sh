e82180a18fea398fdad4ce3f7848307a04bae613
baimamboukar <baimamboukar@gmail.com>
2022-01-06 23:17:25 +0100

    committing com

--- commit.sh
+++ commit.sh
@@ -1,19 +1,18 @@
 # commit all files in current directory
-cd src
+cd com/baimamboukar/java/fourth_assignment/src
 for file in *
 do
-    echo $file
     git add $file
-    git commit -m "commit $file"
+    git commit -m "committing $file"
     git push
 done
 
-cd - 
-cd class
-for file in *
-do
-    echo $file
-    git add $file
-    git commit -m "compiled $file"
-    git push
-done
+# cd - 
+# cd class
+# for file in *
+# do
+#     echo $file
+#     git add $file
+#     git commit -m "compiled $file"
+#     git push
+# done
